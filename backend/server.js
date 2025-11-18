8deba42b207820beb63e841a4aef8aca0f491fda
prashant khadka <khadkaprashant8@gmail.com>
2025-11-18 22:31:51 +1100

    vercel configuration

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5174"];
+const allowedOrigins = process.env.FRONTEND_URL;
 
 app.use(express.json());
 app.use(cookieParser());
